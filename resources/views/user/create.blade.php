7636d8f564877006dea26aec77786f3fee148ae4
Rifqi Mubarak <ryuuzumaru@gmail.com>
2023-12-05 15:14:46 +0800

    report_message
    report_message

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -31,8 +31,9 @@
                         <div class="col-5 d-flex align-items-center">
                             <label class="mb-0" for="username">Username</label>
                         </div>
-                        <div class="col-7 d-flex align-items-center">
+                        <div class="col-7 d-flex flex-column">
                             <input type="text" id="username" name="username" class="form-control">
+                            <div class="text-danger text-left" id="error-username"></div>
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -40,49 +41,54 @@
                         <div class="col-5 d-flex align-items-center">
                             <label class="mb-0" for="name">Nama</label>
                         </div>
-                        <div class="col-7 d-flex align-items-center">
+                        <div class="col-7 d-flex flex-column">
                             <input type="text" id="name" name="name" class="form-control">
+                            <div class="text-danger text-left" id="error-name"></div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-5 d-flex align-items-center">
                             <label class="mb-0" for="email">Email</label>
                         </div>
-                        <div class="col-7 d-flex align-items-center">
+                        <div class="col-7 d-flex flex-column">
                             <input type="email" id="email" name="email" class="form-control">
+                            <div class="text-danger text-left" id="error-email"></div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-5 d-flex align-items-center">
                             <label class="mb-0" for="iddinas">Instansi</label>
                         </div>
-                        <div class="col-7 d-flex align-items-center">
+                        <div class="col-7 d-flex flex-column">
                             <select class="form-control select2bs4" id="iddinas" name="id_dinas">
                                 <option value="">-- Pilih Instansi --</option>
                                 @foreach ($dinas as $din)
                                     <option value="{{ $din->id }}">{{ $din->nama }}</option>
                                 @endforeach
                             </select>
+                            <div class="text-danger text-left" id="error-id_dinas"></div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-5 d-flex align-items-center">
                             <label class="mb-0" for="noHp">Nomor Hp</label>
                         </div>
-                        <div class="col-7 d-flex align-items-center">
-                            <input type="noHp" id="noHp" name="noHp" class="form-control">
+                        <div class="col-7 d-flex flex-column">
+                            <input type="tel" id="noHp" name="noHp" pattern="[0-9]{12,13}" placeholder="081234567809" class="form-control">
+                            <div class="text-danger text-left" id="error-noHp" ></div>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col-5 d-flex align-items-center">
                             <label class="mb-0" for="role">Peran</label>
                         </div>
-                        <div class="col-7 d-flex align-items-center">
+                        <div class="col-7 d-flex flex-column">
                             <select class="form-control select2bs4" id="role" name="role">
                                 <option value="">-- Pilih peran --</option>
                                 <option value="produsen">Produsen</option>
                                 <option value="admin">Admin</option>
                             </select>
+                            <div class="text-danger text-left" id="error-role"></div>
                         </div>
                     </div>
             </div>
@@ -93,7 +99,7 @@
                     <div class="col-5 d-flex align-items-center">
                         <label class="mb-0" for="password">Password</label>
                     </div>
-                    <div class="col-7 d-flex align-items-center">
+                    <div class="col-7 d-flex flex-column">
                         <input type="password" id="password" name="password" class="form-control">
                     </div>
                 </div>
@@ -101,13 +107,15 @@
                     <div class="col-5 d-flex align-items-center">
                         <label class="mb-0" for="password_confirmation">Konfirmasi Password</label>
                     </div>
-                    <div class="col-7 d-flex align-items-center">
+                    <div class="col-7 d-flex flex-column">
                         <input type="password" id="password_confirmation" name="password_confirmation"
                             class="form-control">
+                        <div class="text-danger text-left" id="error-password"></div>
                     </div>
                 </div>
                 <div class="text-right">
-                    <button id="addUsers" class="btn btn-sm btn-info"> <i class="fa-solid fa-save"></i> Simpan</button>
+                    <button id="addUsers" class="btn btn-sm btn-info"> <i class="fa-solid fa-save"></i>
+                        Simpan</button>
                 </div>
             </div>
         </div>
@@ -119,7 +127,6 @@
             const add_URL = new URL("{{ route('users.add') }}")
             const this_URL = new URL("{{ route('users.index') }}")
             const tokens = "{{ csrf_token() }}"
-
         </script>
     </x-slot>
 </x-niu-layout>
